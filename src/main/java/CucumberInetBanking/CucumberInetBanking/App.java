43602bd79eec4e1f016857012e46f64f9a9c3681
purna <purnapcn@gmail.com>
2019-05-05 20:04:55 +0530

    FirstPush

---
+++ src/main/java/CucumberInetBanking/CucumberInetBanking/App.java
@@ -0,0 +1,13 @@
+package CucumberInetBanking.CucumberInetBanking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
